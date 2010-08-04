17608c2b6a782c5fcf0c9f8f2a768a37a2ad2f9c
Max Usachev <maxusachev@gmail.com>
2010-08-04 16:56:27 +0300

    updated UI factory

--- factory.py
+++ factory.py
@@ -4,10 +4,10 @@ UI factory
 
 def app_factory(interface=None):
     if not interface or interface == 'fremantle':
-        from diabetto.ui.fremantle_ui.fremantle_ui import FremantleDiabetto
-        return FremantleDiabetto
+        from diabetto.ui.fremantle_ui.fremantle_ui import MainWidget
+        return MainWidget
     elif interface == 'diablo':
-        from diabetto.ui.diablo_ui.diablo_ui import DiabloDiabetto
-        return DiabloDiabetto
+        from diabetto.ui.diablo_ui.diablo_ui import MainWidget
+        return MainWidget
     else:
         raise ValueError('%s: unsupported UI type' % interface)
